f164389de388ed8325490c6b3d6161a57faf9db1
Fahrezy22 <fahriza2222@gmail.com>
2023-09-09 10:36:41 +0700

    make raw func

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Letter extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id_jenis_surat',
+        'nama_pemohon',
+        'tgl_surat',
+        'nomor_surat',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backoffice\LetterController;
 use App\Http\Controllers\Backoffice\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,8 @@ Route::prefix('v1/users')->controller(UserController::class)->group(function() {
     Route::get('/', 'getAllData');
     Route::get('/{id}', 'getDataById');
     Route::post('/', 'upsertData');
+});
+
+Route::prefix('v1/letter')->controller(LetterController::class)->group(function() {
+    Route::post('/', 'createPayload');
 });
