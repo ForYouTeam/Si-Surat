2c0ddce712a6957092f6f03e295df8a2a6681911
nzrkodi <nzrkodi@gmail.com>
2023-09-10 17:26:44 +0800

    setup template

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,24 @@
+  <nav class="bg-white sidebar sidebar-offcanvas mt-3" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item ">
+        <b>HOME</b>
+        <a class="nav-link {{Route::is()}}" href="{{route('dashboard')}}">
+          <img src="{{asset('assets/images/icons/1.png')}}" alt="">
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{route('bo-surat')}}">
+          <img src="{{asset('assets/images/icons/2.png')}}" alt="">
+          <span class="menu-title">Surat</span>
+        </a>
+      </li>
+      <li class="nav-item"><hr>
+        <b class="text-gray">MANAGE USER</b>
+        <a class="nav-link" href="{{route('bo-akun')}}">
+          <img src="{{asset('assets/images/icons/10.png')}}" alt="">
+          <span class="menu-title">Account</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
