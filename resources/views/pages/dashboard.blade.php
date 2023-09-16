715b0f037e6b5742ae56226b7d33c45f39154af0
nzrkodi <nzrkodi@gmail.com>
2023-09-16 15:30:28 +0800

    update sedebar

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,53 +5,82 @@
 @section('content')
     <div class="row">
         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
-            <div class="card card-statistics">
-                <div class="card-body">
-                    <div class="clearfix">
-                        <p class="card-text text-dark">SKTM</p>
-                        <h4 class="bold-text">{{$data['sktm']}}</h4>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-title font-weight-normal text-primary">{{$data['sktm']}}</h4>
+                        <h6 class="card-subtitle mb-4">Surat KTM</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <i class="fa fa-envelope float-right icon-grey-big text-success"></i>
                     </div>
                 </div>
+              </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
-            <div class="card card-statistics">
-                <div class="card-body">
-                    <div class="clearfix">
-                        <p class="card-text text-dark">Surat Keterangan Domisili</p>
-                        <h4 class="bold-text">{{$data['domisili']}}</h4>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-title font-weight-normal text-primary">{{$data['domisili']}}</h4>
+                        <h6 class="card-subtitle mb-4">Surat Domisili</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <i class="fa fa-envelope float-right icon-grey-big text-primary"></i>
                     </div>
                 </div>
+              </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
-            <div class="card card-statistics">
-                <div class="card-body">
-                    <div class="clearfix">
-                        <p class="card-text text-dark">Surat Keputusan</p>
-                        <h4 class="bold-text">{{$data['keputusan']}}</h4>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-title font-weight-normal text-primary">{{$data['keputusan']}}</h4>
+                        <h6 class="card-subtitle mb-4">Surat Keputusan</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <i class="fa fa-envelope float-right icon-grey-big text-danger"></i>
                     </div>
                 </div>
+              </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
-            <div class="card card-statistics">
-                <div class="card-body">
-                    <div class="clearfix">
-                        <p class="card-text text-dark">Surat Tanda Terima</p>
-                        <h4 class="bold-text">{{$data['tt']}}</h4>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-title font-weight-normal text-primary">{{$data['tt']}}</h4>
+                        <h6 class="card-subtitle mb-4">Surat Tanda Terima</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <i class="fa fa-envelope float-right icon-grey-big text-warning"></i>
                     </div>
                 </div>
+              </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
-            <div class="card card-statistics">
-                <div class="card-body">
-                    <div class="clearfix">
-                        <p class="card-text text-dark">Surat Undangan</p>
-                        <h4 class="bold-text">{{$data['undangan']}}</h4>
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4 class="card-title font-weight-normal text-primary">{{$data['undangan']}}</h4>
+                        <h6 class="card-subtitle mb-4">Surat Undangan</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <i class="fa fa-envelope float-right icon-grey-big text-dark"></i>
                     </div>
                 </div>
+              </div>
             </div>
         </div>
     </div>
